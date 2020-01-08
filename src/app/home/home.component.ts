50932de547bf6f6b6053e5d199b5e4373476ba51
Mazen94 <38365486+Mazen94@users.noreply.github.com>
2020-01-08 22:29:57 +0100

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  myArray = ['Peindre un chambre', "Cours d'informatique", 'Monter un meuble', 'Fixer une étagère', 'Garde chien',''];
+    i=0;
+    j;
+     intervalle=setInterval(()=>
+    {
+      if(this.i<4)
+      this.i ++ ;
+      else
+     this.i=0;
+    },2000)
+   
+  constructor() {
+   this.intervalle;}
 
   ngOnInit() {
+  }
+  search()
+  {
+    this.j=this.i;
+    this.i=5;
+    clearInterval(this.intervalle);
+    
   }
 
 }
